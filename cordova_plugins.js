e4f7bb7f98f8e8271936b71bd4dca38f66872bce
mark anthony elbambo <markelbambo@narrasystems.com>
2014-04-01 17:50:30 +0800

    mas bagong cordova

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -1,17 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/org.apache.cordova.device/www/device.js",
-        "id": "org.apache.cordova.device.device",
-        "clobbers": [
-            "device"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "org.apache.cordova.device": "0.2.8"
-}
+{}
 // BOTTOM OF METADATA
 });
